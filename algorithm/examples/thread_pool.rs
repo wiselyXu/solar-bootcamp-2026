3d79a9f010733ca50172592cdaa4c6007219035e
wesley <twtqmq1@163.com>
2026-03-13 07:41:42 +0000

    finish thread pool design

--- algorithm/examples/thread_pool.rs
+++ algorithm/examples/thread_pool.rs
@@ -1,10 +1,38 @@
-use std::thread::{self, Thread};
+use std::{
+    sync::{
+        Arc, Mutex,
+        mpsc::{self, Receiver},
+    },
+    thread::{self, Thread},
+};
 
-pub struct ThreadPool;
-// {
-//   //  threads: Vec<thread::JoinHandle<()>>,
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
 
-// }
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("worker {id} got a job; executing");
+                job();
+            }
+            //receiver;
+        });
+
+        Worker { id, thread }
+    }
+}
+
+//struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
 fn main() {
     println!("ThreadPool example running.");
@@ -13,15 +41,24 @@ fn main() {
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        ThreadPool
+        let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let new_receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&new_receiver)));
+            // create somet threads and store them in the vector
+        }
+
+        ThreadPool { workers, sender }
     }
 
-    pub fn execute<F, T>(&self, f: F)
+    pub fn execute<F>(&self, f: F)
     // 其实 spawn 中没有  &spawn
     where
-        F: FnOnce() -> T,
-        F: Send + 'static,
-        T: Send + 'static,
+        F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
